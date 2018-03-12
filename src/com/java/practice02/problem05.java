e0130f25e6c244de7693a07e32ec881ad8d8b67b
KWAK <KWAK@61.82.99.154>
2018-03-13 01:44:50 +0900

    I did it!

--- src/com/java/practice02/problem05.java
+++ src/com/java/practice02/problem05.java
@@ -5,38 +5,33 @@ public class problem05 {
 	public static void main(String[] args) {
 
 		int[] lotto = new int[6];
-		
+
 		for (int i = 0; i < lotto.length; i++) {
 
-			lotto[i] = (int) (Math.random() * 45 ) + 1;
-
+			lotto[i] = (int) (Math.random() * 45) + 1;
 		}
 
-		for (int i = 0; i < lotto.length; i++) {
+		for (int i = 0; i < lotto.length; i++) {			//2개의 for문을 써서 lotto[i]==lotto[k] 모든 조합 비교 
 
-			
 			for (int k = 1; k <= lotto.length; k++) {
 
 				{
-			
-					if (lotto[i] == lotto[(lotto.length - k)]  &&  i!=(lotto.length-k) ) {
+					if (lotto[i] == lotto[(lotto.length - k)] && i != (lotto.length - k)) {
 
 						for (int n = 0; n < lotto.length; n++) {
 
-							lotto[n] = (int) (Math.random() * 45) + 1;
-							
+							lotto[n] = (int) (Math.random() * 45) + 1;		// 한 조합이라도 중복이면 다시 전체 랜덤
 						}
-						
-						System.out.println("mix");
-						i=0;
-						
-						continue;
-						
+
+						System.out.println("mix");		// 중복이 생겨 리셋 될때마다 mix 표시 
+						i = 0;
+
+						continue;					// continue는 자신이 속한 반복문을 다시 처음부터 반복 시킨다.(k=1부터) 
 					}
 				}
 			}
 		}
-		
+
 		for (int i = 0; i < lotto.length; i++) {
 
 			System.out.print(lotto[i] + " ");
