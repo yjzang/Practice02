a86b2c6a8dac61e096da783e66e0e6eb1b65e065
KWAK <KWAK@61.82.99.154>
2018-03-13 01:22:23 +0900

    I did it!

--- src/com/java/practice02/problem04.java
+++ src/com/java/practice02/problem04.java
@@ -1,18 +1,41 @@
 package com.java.practice02;
 
+import java.util.Scanner;
+
 public class problem04 {
 
 	public static void main(String[] args) {
 
-	
-		int[] money = new int[8];
-		
-		
-		money[i]
-				
-	
-	
-	
+		Scanner sc = new Scanner(System.in);
+
+		int[] unitArr = new int[10];
+		int[] countArr = new int[10];
+
+		int total = sc.nextInt();
+
+		unitArr[0] = 50000;
+		unitArr[1] = 10000;
+		unitArr[2] = 5000;
+		unitArr[3] = 1000;
+		unitArr[4] = 500;
+		unitArr[5] = 100;
+		unitArr[6] = 50;
+		unitArr[7] = 10;
+		unitArr[8] = 5;
+		unitArr[9] = 1;
+
+		for (int i = 0; i < unitArr.length; i++) {
+
+			countArr[i] = total / unitArr[i];
+
+			total = total - unitArr[i] * countArr[i];
+
+		}
+		for (int i = 0; i < unitArr.length; i++) {
+
+			System.out.println(unitArr[i] + "원: " + countArr[i] + "개");
+		}
+		sc.close();
 	}
 
 }
--- src/com/java/practice02/problem05.java
+++ src/com/java/practice02/problem05.java
@@ -1,30 +1,46 @@
 package com.java.practice02;
 
-
 public class problem05 {
 
 	public static void main(String[] args) {
 
-	
-	
 		int[] lotto = new int[6];
 		
-		
-		for(int i=0; i<lotto.length; i++) {
-		
-			lotto[i]= (int)(Math.random()*45) + 1;
+		for (int i = 0; i < lotto.length; i++) {
+
+			lotto[i] = (int) (Math.random() * 45 ) + 1;
+
+		}
+
+		for (int i = 0; i < lotto.length; i++) {
+
 			
+			for (int k = 1; k <= lotto.length; k++) {
+
+				{
+			
+					if (lotto[i] == lotto[(lotto.length - k)]  &&  i!=(lotto.length-k) ) {
+
+						for (int n = 0; n < lotto.length; n++) {
+
+							lotto[n] = (int) (Math.random() * 45) + 1;
+							
+						}
+						
+						System.out.println("mix");
+						i=0;
+						
+						continue;
+						
+					}
+				}
+			}
 		}
 		
-		
-		for(int k=0; k<lotto.length; k++)
-			
-			if(lotto[k]==lotto[k+1]) {
-			
-			  
-			}
-		  
-	
-	}
+		for (int i = 0; i < lotto.length; i++) {
 
+			System.out.print(lotto[i] + " ");
+
+		}
+	}
 }
